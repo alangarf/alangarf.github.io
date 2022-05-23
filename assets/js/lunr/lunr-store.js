f47db231729c08ebccea82b92557e0448b888022
Alan Garfield <alan@fromorbit.com>
2022-05-24 08:32:45 +1000

    updated js and css

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,175 @@
+var store = [{
+        "title": "LMD18200 Breadboard Adapter Board",
+        "excerpt":"I designed this board out of necessity. I needed a decent H-bridge to test my new servo motor controller design without having to commit the entire design to PCB before I could test it. The adapter board uses a single LMD18200 which is a very capable H-Bridge driver. The LMD18200...","categories": [],
+        "tags": [],
+        "url": "/2004/12/08/lmd18200-breadboard-adapter-board.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Image to Gcode",
+        "excerpt":"Been playing with some code to convert images to GCode.   Here is a test sample. Not bad I reckon. From 3 metres away it looks like a black and white photo. :)            ","categories": [],
+        "tags": [],
+        "url": "/2011/08/06/image-to-gcode.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Simple Hartley Oscillator",
+        "excerpt":"I decided recently that RF was a field I had very little practical knowledge of. I know the basics, the methods and the general ideas, but I’ve never actually put together a circuit other than a pre-purchased kit. Thanks Alan Yates and the guys on twitter I set out to...","categories": [],
+        "tags": [],
+        "url": "/2011/11/08/simple-hartley-oscillator.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "CNC drilling lots and lots of holes!",
+        "excerpt":"My friend Alan Yates is currently working through all manner or particle detectors. His latest one is a Gas Electron Multiplier. It’s basically a very two very thin plates with many tiny holes that electrons can go through triggering an electron avalanche between the two highly charged plates. He was...","categories": [],
+        "tags": [],
+        "url": "/2011/11/09/cnc-drilling-lots-and-lots-of-holes.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "My attempt at the WBR Receiver",
+        "excerpt":"What got me started on wanting to increase my RF knowledge was this article by Dave Richards. I was originally looking for interesting ways to make project cases and happened to find his glorious “double-sided PCB case” technique. The technique has been around for a while, but the circuit he...","categories": [],
+        "tags": [],
+        "url": "/2011/11/09/my-attempt-at-the-wbr-receiver.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "SBC servo control under Linux",
+        "excerpt":"Here is a small update on a project I’m working on for a client. I’ve subsequently dropped the split quadrature encoder set up and added another shaft encoder on the actual lead-screw. So now I can actually use a stepper or a servo and I close the control loop using...","categories": [],
+        "tags": [],
+        "url": "/2011/11/09/sbc-servo-control-under-linux.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "WBR receiver fiddling",
+        "excerpt":"       More fiddling with the WBR receiver. Heaps of fun!  ","categories": [],
+        "tags": [],
+        "url": "/2011/11/10/wbr-receiver-fiddling.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "AVR Transistor Tester / Component Identifier",
+        "excerpt":"Ooh a gadget. While browsing dangerousprototypes.com. I saw an interesting little gadget by Mark Frejek at www.mikrocontroller.net. Mark had made a transistor and component identifier using an AVR ATMEGA8, a few resistors and a 16x2 LCD. Always wanting one of those expensive Atlas testers I was intrigued. I saw a...","categories": [],
+        "tags": [],
+        "url": "/2012/01/01/avr-transistor-tester-component-identifier.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Why Microchip with MPLABX has put the cart before the horse.",
+        "excerpt":"Update: I received a response from Microchip; see the end. The history. Hello friends, let this tale of woe be a lesson for those deciding on a MCU supplier. I had been a long time user of Microchip parts, and a fan for over ten years. I’ve owned and built...","categories": [],
+        "tags": [],
+        "url": "/2012/01/06/why-microchip-with-mplabx-has-put-the-cart-before-the-horse.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Wishbone FPGA Motion Controller",
+        "excerpt":"       This is just showing off my little Wishbone motion controller I wrote for this little TS7500 SBC.  ","categories": [],
+        "tags": [],
+        "url": "/2012/09/05/wishbone-fpga-motion-controller.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "10-turn Potentiometer Tear Down",
+        "excerpt":"       I pull apart a 10-turn potentiometer to see what’s inside. Pretty cool.  ","categories": [],
+        "tags": [],
+        "url": "/2012/09/18/10-turn-potentiometer-tear-down.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Web interface for Motion controller",
+        "excerpt":"       This is a quick show-and-tell of the working of my motion controller.  ","categories": [],
+        "tags": [],
+        "url": "/2012/12/14/web-interface-for-motion-controller.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Tiny Matrix Hack",
+        "excerpt":"       This is a PCB mounted version of the TinyMatrix. I built it for my little nephew for Christmas. I opted to use AAA batteries as CR2032’s aren’t that common for a kids toy.  ","categories": [],
+        "tags": [],
+        "url": "/2012/12/29/tiny-matrix-hack.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Bluetooth fun",
+        "excerpt":"       Mucking around on a Sunday with the excellent Bluegiga WT32 module.  ","categories": [],
+        "tags": [],
+        "url": "/2013/05/18/bluetooth-fun.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Initial ZPU muck-about",
+        "excerpt":"       This is a fairly boring video of me talking to camera about my ideas on using a ZPU softcore CPU for a project I have on the boil. There is also a bit of a rant about crappy operating systems and vendor tools.  ","categories": [],
+        "tags": [],
+        "url": "/2013/05/25/initial-zpu-muck-about.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Papilio DX board",
+        "excerpt":"       I found a cool discovery while talking on the phone and fiddling with things on my bench. The cool little Papilio One FPGA board has a perfect fitting random development board from Deal Extreme that I just happen to find. They still seem readily available and super useful!  ","categories": [],
+        "tags": [],
+        "url": "/2013/10/27/papilio-dx-board.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "The Mini H Quad v2.0 “The Hammer” frame",
+        "excerpt":"       This frame is freely available from http://untestedprototype.com/ and is feature packed for something that’s so small.   I fully intend to FPV this little quad and hopefully be buzzing around my local park.   Can’t wait! Damn international shipping!  ","categories": [],
+        "tags": [],
+        "url": "/2013/11/21/the-mini-h-quad-v2-0-the-hammer-frame.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Digital Foosball",
+        "excerpt":"       We digitized our foosball table for a hackday at work. It was a really fun task that has added a new dimension to our game play.  ","categories": [],
+        "tags": [],
+        "url": "/2014/09/17/digital-foosball.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Digital Foosball Two",
+        "excerpt":"      ","categories": [],
+        "tags": [],
+        "url": "/2014/09/21/digital-foosball-two.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Updating the website...",
+        "excerpt":"Wonders of wonders… Yes my website is being updated once again. After about 5 aborted attempts to rebuild all my old content and get this site up and going again. I’ve finally decided to just start making new content, and if I find the time (and the will) I’ll periodically...","categories": [],
+        "tags": [],
+        "url": "/2017/03/30/updating-website.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Retro Challenge 2017/04",
+        "excerpt":"After working with a friend on a Z80 based project I spoke about some SCC2691 UARTs I had from an old board I had saved from a skip bin some 10 years ago. He asked questions about the board and I described it to him.. “Yeah it’s got a 68000...","categories": [],
+        "tags": [],
+        "url": "/2017/03/31/retro-computing-challenge-201704.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Beeping out the board - Retro Challenge 2017/04",
+        "excerpt":"I first started with this board by pulling off the ROMs and dumping their contents. Being a m68k CPU generally the ROMs are split into odd and even byte streams, and in order to dissassemble them the two ROM images need to be interleaved again. I achieved this with a...","categories": [],
+        "tags": [],
+        "url": "/2017/04/03/m68k-amx-board-rc201704.html",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Got together a schematic today - Retro Challenge 2017/04",
+        "excerpt":"Quick one today, I worked on the schematic a little more. Still a few unknowns and a couple of holes I’ve not filled in yet, but the schematic is 80% complete. At least to a point where I can get a ROM monitor etc running and know where all the...","categories": [],
+        "tags": [],
+        "url": "/2017/04/05/m68k-schematic-rc201704.html",
+        "teaser": "/assets/images/2017-03-31/m68k.png"
+      },{
+        "title": "Ugh! I need UV - Retro Challenge 2017/04",
+        "excerpt":"So I set out to make the LED blink a merry blink tonight, but was finally thwarted when I came to the realisation I’d need UV light to erase the EPROMs to load my code in. Suffice to say this was mighty frustrating. I’m getting ahead of the story, so...","categories": [],
+        "tags": [],
+        "url": "/2017/04/06/m68k-why-no-uv-rc201704.html",
+        "teaser": "/assets/images/2017-03-31/m68k.png"
+      },{
+        "title": "Now we're get'n somewhere! - Retro Challenge 2017/04",
+        "excerpt":"After the UV fiasco I’ve finally managed to get around the issue. I did that by ditching EPROMs and instead replaced them with EEPROMs! Rummaging around in the shed I found two other cards from the system this controller was in originally. They were for controlling infrared devices and what...","categories": [],
+        "tags": [],
+        "url": "/2017/04/08/gettin-somewhere-rc201704.html",
+        "teaser": "/assets/images/2017-04-08/adapters_in_place.jpg"
+      },{
+        "title": "Rinse Repeat. :( - Retro Challenge 2017/04",
+        "excerpt":"Apart from work and family commitments over Easter I’ve been stuck in a rinse repeat cycle with this board. I am still trying to iron the bugs out, and I’m having weird issues with the firmware. I started thinking about porting the TS2 “monitor” to this board, but was having...","categories": [],
+        "tags": [],
+        "url": "/2017/04/18/rinse-repeat-rc201704.html",
+        "teaser": "/assets/images/2017-04-18/axc-em.jpg"
+      },{
+        "title": "m68k Environment - Retro Challenge 2017/04",
+        "excerpt":"So, for this post I want to talk about my development environment and where I’m up to as far as the Retro Challenge. I’ve had a lot of issues with this project. Initially I was cruising well and looking well on track to make something really cool, but then I...","categories": [],
+        "tags": [],
+        "url": "/2017/04/26/m68k-environment-rc201704.html",
+        "teaser": "/assets/images/2017-04-18/mc68340.jpg"
+      },{
+        "title": "More fun with the m68k",
+        "excerpt":"The last days of the Retro Challenge The Retro Challenge behind me, and not actually achieving my goal I was pretty disappointed that work/life commitments stacked against me in getting there. That didn’t stop me from completing my goal, just wasn’t in the time frame of the competition. Where I...","categories": [],
+        "tags": [],
+        "url": "/2017/05/19/m68k-crosstool-ng.html",
+        "teaser": "/assets/images/2017-04-18/mc68340.jpg"
+      },{
+        "title": "45W IRF510 HF Linear Amp",
+        "excerpt":"45W IRF510 HF Linear Amp Boy it’s been a long time between posts. Sorry about that. I got real lazy and just starting pumping my content into twitter and really just never took the time to document things. Anyway, here is a project I built to get me out of...","categories": [],
+        "tags": [],
+        "url": "/2022/05/23/IRF510-linear-amp.html",
+        "teaser": "/assets/images/2022-05-23/linear16.jpg"
+      }]
